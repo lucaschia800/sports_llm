982d2b8fc0933d6e3baee6a01de920d856036e97
lucaschia800 <lucaschia800@gmail.com>
2025-02-09 20:25:35 -0800

    adding some files

--- test_toke.py
+++ test_toke.py
@@ -1,5 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from datasets import Dataset
+from datasets import Dataset, load_dataset
+
+
 
 
 ds = Dataset.from_dict({'text': [('Also, using batched processing might be more efficient. Wait, if the user processes examples in batches, can we handle that? Maybe not, because each text in the batch might have different lengths. So processing one example at a time (batched=False) might be easier, even if less efficient. But for large datasets, this could be slow. However, given that the user is dealing with continued pre-training, which often involves large datasets, efficiency is important.'
@@ -11,11 +13,11 @@ ds = Dataset.from_dict({'text': [('Also, using batched processing might be more 
                                     'Also, using batched processing might be more efficient. Wait, if the user processes examples in batches, can we handle that? Maybe not, because each text in the batch might have different lengths. So processing one example at a time (batched=False) might be easier, even if less efficient. But for large datasets, this could be slow. However, given that the user is dealing with continued pre-training, which often involves large datasets, efficiency is important.'
                                     'Also, using batched processing might be more efficient. Wait, if the user processes examples in batches, can we handle that? Maybe not, because each text in the batch might have different lengths. So processing one example at a time (batched=False) might be easier, even if less efficient. But for large datasets, this could be slow. However, given that the user is dealing with continued pre-training, which often involves large datasets, efficiency is important.')]})
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B")
 print(tokenizer.model_max_length)
 tokenizer.pad_token = tokenizer.eos_token
 
 ds_tokenized = ds.map(lambda x: tokenizer(x['text'], padding="max_length", truncation=True), batched=True)
 
 
-print(ds_tokenized[0])
+print(ds_tokenized[0].keys())
