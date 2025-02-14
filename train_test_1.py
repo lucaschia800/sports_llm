59e6b31c2df8f57d4ce4b4195a6b9f56b8e6028e
lucaschia800 <lucaschia800@gmail.com>
2025-02-13 22:07:46 -0800

    lora update

--- train_test_1.py
+++ train_test_1.py
@@ -4,15 +4,18 @@ from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
 from huggingface_hub import login
 import os
+from peft import LoraConfig, TaskType, get_peft_model
 
 #login to huggingface
 
 hf_token = os.getenv('HUGGINGFACE_TOKEN')
 
 if hf_token:
-    login(token = hf_token)
+    login(token=hf_token)  # Login with token
+else:
+    raise ValueError("HUGGINGFACE_TOKEN not found! Set it in your SLURM script.")
 
-BATCH_SIZE = 64
+BATCH_SIZE = 2
 
 def get_model_and_tok():
     """Instantiating model and tokenizer"""
@@ -21,15 +24,20 @@ def get_model_and_tok():
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map = 'auto')
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-
+    tokenizer.pad_token = tokenizer.eos_token
     return model, tokenizer
+
+
+def get_peft_model(model, config):
+
+    return get_peft_model(model, config)
 
 
 
 def load_datasets():
     """
     Loading in the datasets
-    
+
     """
     ds = load_dataset('GEM/sportsett_basketball')
     ds_train = ds['train']
@@ -39,7 +47,7 @@ def load_datasets():
     return ds_train, ds_val, ds_test
 
 
-def preprocess_function(examples, tokenizer):
+def preprocess_function(examples):
     #for now we are just tokenizing without padding but if it turns out llama doesn't accept then we will pad
     return tokenizer(examples["target"], truncation=True)
 
@@ -82,17 +90,25 @@ def get_trainer(model, tokenizer, ds_train, ds_val):
 if __name__ == "__main__":
     model, tokenizer = get_model_and_tok()
 
+
+    Lora_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        target_modules = ['q_proj', 'k_proj', 'v_proj', 'out_proj', 'up_proj', 'down_proj'],  #start with training all change if overfitting
+        r = 16,
+        lora_alpha = 32, # typically double or equal to r, it is the lr scaling factor
+        lora_droput = 0.01,
+    )
+
+    lora_model = get_peft_model(model, Lora_config)
+
+
     ds_train, ds_val, ds_test = load_datasets()
 
     ds_train_tokenized = process_dataset(ds_train)
     ds_val_tokenized = process_dataset(ds_val)
 
-    trainer = get_trainer(model, tokenizer, ds_train_tokenized, ds_val_tokenized)
+    
+
+    trainer = get_trainer(lora_model, tokenizer, ds_train_tokenized, ds_val_tokenized)
 
     trainer.train()
-
-        
-
-
-
-
